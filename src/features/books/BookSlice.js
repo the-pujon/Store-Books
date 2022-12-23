9d52032bc6c77d3192458a1f3b001d4ab32fb4e2
Pujon Das Auvi <auvipujondas@gmail.com>
2022-12-23 14:12:57 +0600

    explained how previous issue solved and rename title

--- src/features/books/BookSlice.js
+++ src/features/books/BookSlice.js
@@ -1,5 +1,8 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
+//here I used localStorage getItem for get the item thats stored in localStorage already.
+//But if there is no data in localStorage then it will take those data that i defined ...
+
 const initialBooks = {
   books: JSON.parse(localStorage.getItem("initBooks")) || [
     { id: 1, title: "Cokher bali", author: "Pujon Das Auvi" },
@@ -15,12 +18,21 @@ export const BooksSlice = createSlice({
     showBooks: (state) => state,
     addBook: (state, action) => {
       state.books.push(action.payload);
+
+      //after pushing new book into the state I set this array to localStorage by localStorage.setItem
       localStorage.setItem("initBooks", JSON.stringify(state.books));
     },
     updateBook: (state, action) => {
       const { id, title, author } = action.payload;
+
+      //here First we need to filter the book that we want to edit or update whatever you say.
       const existBook = state.books.filter((book) => book.id === id);
+      //then store the book id in bookId
       const bookId = existBook[0].id;
+
+      //here we run a for loop to find is bookId match with our state or not..
+      //if it match with state then set new title and author to our state
+      //and after that we again store our state in localStorage
 
       for (let i = 0; i < state.books.length; i++) {
         if (state.books[i].id === bookId) {
@@ -35,6 +47,8 @@ export const BooksSlice = createSlice({
       const id = action.payload;
 
       state.books = state.books.filter((book) => book.id !== id);
+
+      //after filtering book we again store our state in localStorage
       localStorage.setItem("initBooks", JSON.stringify(state.books));
     },
   },
